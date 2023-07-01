d6b880e64bcd6c6cfa458543ab7b6df69ab06a79
shin-mallang <huipulci1@naver.com>
2023-07-02 01:11:11 +0900

    fix: MemberRepository의 getById 예외처리 (#3)

--- src/main/java/com/vestie/vestie/member/domain/MemberRepository.java
+++ src/main/java/com/vestie/vestie/member/domain/MemberRepository.java
@@ -1,5 +1,8 @@
 package com.vestie.vestie.member.domain;
 
+import static com.vestie.vestie.member.exception.MemberExceptionType.NOT_FOUND_MEMBER;
+
+import com.vestie.vestie.member.exception.MemberException;
 import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +11,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Nonnull
     default Member getById(Long id) {
         return findById(id)
-                .orElseThrow();
+                .orElseThrow(() -> new MemberException(NOT_FOUND_MEMBER));
     }
 }
