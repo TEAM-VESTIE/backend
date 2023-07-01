2e0b0e21b8777bcabbebe1e1346177ed85aca352
Donghun Shin <huipulci1@naver.com>
2023-07-02 00:41:34 +0900

    chore: 개발 환경을 세팅한다 (#3)
    * chore: .gitignore 추가 (#1)
    
    * chore: 프로젝트 초기 환경 구성 (#1)

---
+++ src/main/java/com/vestie/vestie/VestieApplication.java
@@ -0,0 +1,13 @@
+package com.vestie.vestie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VestieApplication.class, args);
+	}
+
+}
