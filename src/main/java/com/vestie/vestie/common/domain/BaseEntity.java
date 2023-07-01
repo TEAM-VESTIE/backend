595bba1ee3eb62777d7e51acb5f4ff165e0dca4e
Donghun Shin <huipulci1@naver.com>
2023-07-02 00:56:15 +0900

    feat: 공통으로 사용할 클래스들을 정의한다 (#5)
    * feat: 공용 엔티티 클래스 작성 (#2)
    
    * feat: JPA 설정 클래스 작성 (#2)
    
    * feat: 공통 예외 클래스 작성 (#2)

---
+++ src/main/java/com/vestie/vestie/common/domain/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.vestie.vestie.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    protected BaseEntity() {
+    }
+
+    public BaseEntity(Long id) {
+        this.id = id;
+    }
+
+    public Long id() {
+        return id;
+    }
+
+    public LocalDateTime createdDate() {
+        return createdDate;
+    }
+}
---
+++ src/main/java/com/vestie/vestie/common/exception/ExceptionControllerAdvice.java
@@ -0,0 +1,48 @@
+package com.vestie.vestie.common.exception;
+
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+
+    private static final String UNEXPECTED_ERROR_CODE = "1";
+    private static final String BAD_REQUEST_ERROR_CODE = "2";
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ExceptionResponse> handleException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getFieldErrors().stream()
+                .map(it -> it.getField() + " : " + it.getDefaultMessage())
+                .collect(Collectors.joining("\n"));
+        log.warn("요청 필드의 형식이 올바르지 않습니다. [%s]".formatted(errorMessage));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                BAD_REQUEST_ERROR_CODE,
+                "요청 필드의 형식이 올바르지 않습니다. [%s]".formatted(errorMessage));
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(BaseException.class)
+    ResponseEntity<ExceptionResponse> handleException(BaseException e) {
+        BaseExceptionType type = e.exceptionType();
+        log.warn("[WARM] 예외 내용: {}", type.errorMessage());
+        return new ResponseEntity<>(
+                new ExceptionResponse(String.valueOf(type.errorCode()), type.errorMessage()),
+                type.httpStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        log.error("[ERROR] 원인을 알 수 없는 예외", e);
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse(
+                        UNEXPECTED_ERROR_CODE,
+                        "알 수 없는 오류가 발생했습니다."));
+    }
+}
