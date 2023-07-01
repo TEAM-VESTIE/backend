723420bf254a826df83c88221afa1b43425df882
shin-mallang <huipulci1@naver.com>
2023-07-01 23:55:36 +0900

    feat: JPA 설정 클래스 작성 (#2)

---
+++ src/main/java/com/vestie/vestie/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.vestie.vestie.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
