ce3e41e1af097c2fd1e4aa0e0d1b43c8d52807b3
KwakMirae <alfo9490@naver.com>
2023-08-08 22:09:08 +0900

    test: 설문 조회 테스트 작성

--- src/test/java/com/vestie/vestie/survey/application/SurveyServiceTest.java
+++ src/test/java/com/vestie/vestie/survey/application/SurveyServiceTest.java
@@ -3,6 +3,7 @@ package com.vestie.vestie.survey.application;
 import static com.vestie.vestie.member.fixture.MemberFixture.동훈;
 import static com.vestie.vestie.survey.domain.MultipleChoiceQuestion.ChoiceConstraints.MULTIPLE;
 import static com.vestie.vestie.survey.domain.MultipleChoiceQuestion.ChoiceConstraints.SINGLE;
+import static com.vestie.vestie.survey.fixture.SurveyFixture.설문;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.vestie.vestie.common.DataClearExtension;
@@ -17,11 +18,11 @@ import com.vestie.vestie.survey.domain.Survey;
 import com.vestie.vestie.survey.domain.SurveyRepository;
 import java.time.LocalDateTime;
 import java.util.List;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
+
+import com.vestie.vestie.survey.presentation.dto.SurveyInquiryResponse;
+import com.vestie.vestie.survey.presentation.dto.SurveyResponse;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -37,6 +38,9 @@ class SurveyServiceTest {
 
     @Autowired
     private SurveyService surveyService;
+
+    @Autowired
+    private SurveyQueryService surveyQueryService;
 
     @Autowired
     private MemberRepository memberRepository;
@@ -103,46 +107,74 @@ class SurveyServiceTest {
         }
     }
 
-//    @Nested
-//    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//    class 설문조회_시 {
-//
-//        private Member member;
-//        private Survey survey;
-//
-//        @BeforeAll
-//        void setup() {
-//            Member 회원 = 동훈();
-//            member = memberRepository.save(회원);
-//
-//            // 설문 등록
-//            survey = surveyRepository.save(설문());
-//            surveyRepository.save(설문());
-//            surveyRepository.save(설문());
-//        }
-//
-//        @Test
-//        void 설문_리스트가_조회된다() {
-//            // when
-//            List<SurveyInquiryResponse> allSurvey = surveyQueryService.getAllSurvey();
-//
-//            // then
-//            assertThat(allSurvey.get(0))
-//                    .usingRecursiveComparison()
-//                    .ignoringFields("id")
-//                    .isEqualTo(survey);
-//        }
-//
-//        @Test
-//        void 설문_하나가_조회된다() {
-//            // when
-//            SurveyResponse getSurvey = surveyQueryService.getSurvey(survey.id());
-//
-//            // then
-//            assertThat(getSurvey)
-//                    .usingRecursiveComparison()
-//                    .ignoringFields("id")
-//                    .isEqualTo(survey);
-//        }
-//    }
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class 설문조회_시 {
+
+        private Member member;
+        private Survey survey;
+
+        @BeforeEach
+        void setup() {
+            Member 회원 = 동훈();
+            member = memberRepository.save(회원);
+
+            // 설문 등록
+            SurveyRegisterCommand surveyRegisterCommand = new SurveyRegisterCommand(
+                    회원.id(),
+                    "Vestie 설문조사",
+                    "Vestie 에 대한 설문 조사입니다.",
+                    LocalDateTime.now().plusDays(10),
+                    List.of(
+                            new SubjectiveQuestionCommand("이름을 알려주세요"),
+                            new MultipleChoiceQuestionCommand("몇학년인가요?", SINGLE, List.of(
+                                    "1학년",
+                                    "2학년",
+                                    "3학년",
+                                    "4학년",
+                                    "기타"
+                            )),
+                            new MultipleChoiceQuestionCommand("취미가 무엇인가요? (복수 선택 가능)", MULTIPLE, List.of(
+                                    "게임",
+                                    "코딩",
+                                    "여행",
+                                    "기타"
+                            )),
+                            new SubjectiveQuestionCommand("재학중인 학과를 입력해주세요"),
+                            new SubjectiveQuestionCommand("MBTI를 입력해주세요")
+                    )
+            );
+
+            Long registeredId = surveyService.register(surveyRegisterCommand);
+            survey = surveyRepository.findById(registeredId).get();
+            surveyService.register(surveyRegisterCommand);
+        }
+
+        @Test
+        void 설문_리스트가_조회된다() {
+            // when
+            List<SurveyInquiryResponse> allSurvey = surveyQueryService.getAllSurvey();
+            // then
+            assertThat(allSurvey.get(0))
+                    .usingRecursiveComparison()
+                    .comparingOnlyFieldsOfTypes(Survey.class)
+                    .ignoringFields("id")
+                    .isEqualTo(survey);
+            assertThat(allSurvey.size())
+                    .isEqualTo(2);
+        }
+
+        @Test
+        void 설문_하나가_조회된다() {
+            // when
+            SurveyResponse getSurvey = surveyService.getSurvey(survey.id());
+
+            // then
+            assertThat(getSurvey)
+                    .usingRecursiveComparison()
+                    .comparingOnlyFieldsOfTypes(Survey.class)
+                    .ignoringFields("id")
+                    .isEqualTo(survey);
+        }
+    }
 }
--- src/test/java/com/vestie/vestie/survey/fixture/SurveyFixture.java
+++ src/test/java/com/vestie/vestie/survey/fixture/SurveyFixture.java
@@ -1,22 +1,28 @@
-//package com.vestie.vestie.survey.fixture;
-//
-//import com.vestie.vestie.member.domain.Member;
-//import com.vestie.vestie.member.fixture.MemberFixture;
-//import com.vestie.vestie.survey.domain.Survey;
-//import java.time.LocalDateTime;
-//
-//@SuppressWarnings("NonAsciiCharacters")
-//public class SurveyFixture {
-//
-//    public static final String 제목 = "베스티 선호도 설문 조사";
-//    public static final LocalDateTime 설문_종료일 = LocalDateTime.now();
-//    public static final Member 동훈 = MemberFixture.동훈();
-//
-//    public static Survey 설문() {
-//        return Survey.builder()
-//                .title(제목)
-//                .endDate(설문_종료일)
-//                .member(동훈)
-//                .build();
-//    }
-//}
+package com.vestie.vestie.survey.fixture;
+
+import com.vestie.vestie.member.domain.Member;
+import com.vestie.vestie.member.fixture.MemberFixture;
+import com.vestie.vestie.survey.domain.Question;
+import com.vestie.vestie.survey.domain.Survey;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@SuppressWarnings("NonAsciiCharacters")
+public class SurveyFixture {
+
+    public static final String 제목 = "베스티 선호도 설문 조사";
+    public static final String 설명 = "베스티 설문조사 앱에 대한 설문조사 입니다.";
+    public static final LocalDateTime 설문_종료일 = LocalDateTime.now();
+    public static final Member 동훈 = MemberFixture.동훈();
+    private static final List<Question> 질문들 = QuestionFixture.질문들();
+
+    public static Survey 설문() {
+        return Survey.builder()
+                .title(제목)
+                .endDate(설문_종료일)
+                .member(동훈)
+                .description(설명)
+                .questions(질문들)
+                .build();
+    }
+}
